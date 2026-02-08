a1ee8a0d771f479eb3a0988a71cfdbf604ad5bdc
rv-bit <robertvilcu89@gmail.com>
2026-02-08 13:18:35 +0000

    feat: add unf table schema

---
+++ unf_table.sql
@@ -0,0 +1,41 @@
+CREATE TABLE kart_racing_unf 
+    ( 
+		-- Customers
+		customer_id INTEGER NOT NULL,
+		customer_first_name VARCHAR2 (50 CHAR) NOT NULL, 
+        customer_last_name VARCHAR2 (50 CHAR) NOT NULL, 
+        customer_email VARCHAR2 (50 CHAR) NOT NULL, 
+        customer_updated_at DATE NOT NULL, 
+        customer_created_at DATE NOT NULL,
+
+		-- Finalised Booked places
+		booked_place_id INTEGER NOT NULL,
+        booked_place_booking_date DATE NOT NULL, 
+        booked_place_quantity INTEGER NOT NULL,
+
+		-- Bookings
+		booking_id INTEGER NOT NULL,
+		booking_status VARCHAR2 (20 CHAR) NOT NULL, 
+        booking_created_at DATE NOT NULL, 
+        booking_reserved_until DATE, 
+
+		-- Time Slots
+        time_slot_id INTEGER NOT NULL, 
+        time_slot_start_time VARCHAR2 (5 CHAR) NOT NULL, 
+        time_slot_end_time VARCHAR2 (5 CHAR) NOT NULL, 
+        time_slot_duration_in_minutes INTEGER NOT NULL,
+
+		-- Track Schedules
+		schedule_id INTEGER NOT NULL, 
+		schedule_day_of_week SMALLINT NOT NULL, 
+        schedule_is_open NUMBER (1) NOT NULL, 
+
+		-- Tracks
+		track_id INTEGER NOT NULL,
+		track_name VARCHAR2 (50 CHAR) NOT NULL, 
+        track_max_karts INTEGER NOT NULL, 
+        track_is_active NUMBER (1) NOT NULL, 
+        track_updated_at DATE NOT NULL, 
+        track_created_at DATE NOT NULL 
+    ) 
+;
