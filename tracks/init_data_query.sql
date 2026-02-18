e136063520a249d106ae6c311b7767b46a6e0ad1
rv-bit <robertvilcu89@gmail.com>
2026-02-18 18:09:07 +0000

    add: 15 more values for tracks

---
+++ tracks/init_data_query.sql
@@ -0,0 +1,17 @@
+INSERT INTO tracks (name, max_karts, is_active, created_at)
+VALUES 
+('Grand Prix Track', 10, 1, SYSDATE()),
+('Speed Circuit', 5, 1, SYSDATE()),
+('Thunder Circuit', 12, 1, SYSDATE()),
+('Lightning Raceway', 8, 1, SYSDATE()),
+('Velocity Track', 15, 1, SYSDATE()),
+('Apex Arena', 10, 1, SYSDATE()),
+('Nitro Loop', 6, 1, SYSDATE()),
+('Drift Kingdom', 14, 1, SYSDATE()),
+('Turbo Circuit', 9, 1, SYSDATE()),
+('Grand Strip', 16, 1, SYSDATE()),
+('Blaze Raceway', 7, 1, SYSDATE()),
+('Rocket Circuit', 13, 1, SYSDATE()),
+('Pole Position Track', 11, 1, SYSDATE()),
+('Ignition Raceway', 8, 1, SYSDATE()),
+('Champion Circuit', 15, 1, SYSDATE());
--- tracks/init_sample_tracks.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO tracks (name, max_karts, is_active, created_at)
-VALUES ('Grand Prix Track', 10, 1, SYSDATE());
-
-INSERT INTO tracks (name, max_karts, is_active, created_at)
-VALUES ('Speed Circuit', 8, 1, SYSDATE());
-
-INSERT INTO tracks (name, max_karts, is_active, created_at)
-VALUES ('Junior Track', 6, 1, SYSDATE());
