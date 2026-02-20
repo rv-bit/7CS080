7af5f067ea8672f4f1a95b1ec7bf79be63133b85
rv-bit <robertvilcu89@gmail.com>
2026-02-20 18:50:20 +0000

    chore: changed comments

--- time_slots/queries.sql
+++ time_slots/queries.sql
@@ -11,7 +11,7 @@ START TRANSACTION;
 	SELECT * FROM time_slots 
 	ORDER BY 1 ASC
 
--- -- Simple delete where first name is CJ, and commit changes
+-- -- Simple delete where id is 7, and commit changes
 START TRANSACTION;
 	DELETE FROM time_slots WHERE id = 7;
 	COMMIT;
