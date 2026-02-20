a97f2c934e05a0967361e5564eaed76196590a28
rv-bit <robertvilcu89@gmail.com>
2026-02-20 19:20:00 +0000

    add: basic update and delete queries

---
+++ track_schedules/queries.sql
@@ -0,0 +1,17 @@
+SET autocommit = 0; -- Causes for no auto committing in MySQL, shouldn't be used in Oracle
+
+-- Simple update where id is 1, and commit changes
+-- If id not found, rollbacks / raises error instantly
+START TRANSACTION;
+	UPDATE track_schedules SET is_open = false
+	WHERE id = 12;
+	COMMIT;
+
+	-- We are getting the latest only change
+	SELECT * FROM track_schedules 
+	ORDER BY 1 ASC
+
+-- -- Simple delete where first name is CJ, and commit changes
+START TRANSACTION;
+	DELETE FROM track_schedules WHERE id = 12;
+	COMMIT;
