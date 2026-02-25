28a5e33ea23e1c8f10b5ba487895756d72232aa7
rv-bit <robertvilcu89@gmail.com>
2026-02-25 21:03:57 +0000

    add: truncate to remove all data from all tables, quick for debugging / testing purposes

---
+++ truncate.sql
@@ -0,0 +1,9 @@
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE bookings;
+TRUNCATE booked_places;
+TRUNCATE customers;
+TRUNCATE time_slots;
+TRUNCATE track_blocks;
+TRUNCATE track_schedules;
+TRUNCATE tracks;
+SET FOREIGN_KEY_CHECKS = 1;
