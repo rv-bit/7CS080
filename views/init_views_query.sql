e362d54ec8bf69b88b011c3fed5110688d506195
rv-bit <robertvilcu89@gmail.com>
2026-03-04 22:35:43 +0000

    add: new view 'v_track_slots' to hold information about tracks joining schedules & time slots & refactor 'v_track_availability_view' to calculate correctly the available_karts

--- views/init_views_query.sql
+++ views/init_views_query.sql
@@ -1,48 +1,47 @@
-CREATE OR REPLACE VIEW v_track_availability_view AS
+CREATE OR REPLACE VIEW v_track_availability_view ( track_id, track_name, max_karts, booking_date, time_slots_id, start_time, end_time, booked_karts, available_karts) AS
     SELECT
         TRK.id AS track_id,
         TRK.name AS track_name,
         TRK.max_karts,
-        BKP.booking_date,
+        CAPTR.booking_date,
         TS.id AS time_slots_id,
         TS.start_time,
         TS.end_time,
-        IFNULL(
-            SUM(
-                CASE
-                    WHEN BKN.status IN ('CONFIRMED', 'PENDING')
-                    THEN BKP.quantity
-                    ELSE 0
-                END
-            ),
-            0
-        ) AS booked_karts,
-        TRK.max_karts
-        - IFNULL(
-                SUM(
-                    CASE
-                        WHEN BKN.status IN ('CONFIRMED', 'PENDING')
-                        THEN BKP.quantity
-                        ELSE 0
-                    END
-            ), 0) AS available_karts
+        COALESCE(CAPTR.booked_karts, 0) AS booked_karts,
+        GREATEST(TRK.max_karts - COALESCE(CAPTR.booked_karts, 0), 0) AS available_karts
     FROM tracks TRK
-    JOIN track_schedules SCH
-        ON SCH.tracks_id = TRK.id
-        AND SCH.is_open = 1
-    JOIN time_slots TS
-        ON TS.id = SCH.time_slots_id
-    LEFT JOIN booked_places BKP
-        ON BKP.tracks_id = TRK.id
-        AND BKP.time_slots_id = TS.id
-    LEFT JOIN bookings BKN
-        ON BKN.id = BKP.bookings_id
-    WHERE TRK.is_active = 1
-    GROUP BY
-        TRK.id,
-        TRK.name,
-        TRK.max_karts,
-        BKP.booking_date,
-        TS.id,
-        TS.start_time,
-        TS.end_time;
+        JOIN track_schedules SCH ON SCH.tracks_id = TRK.id AND SCH.is_open = 1
+        JOIN time_slots TS ON TS.id = SCH.time_slots_id
+
+        LEFT JOIN (
+            SELECT
+                BKP.tracks_id,
+                BKP.time_slots_id,
+                BK.booking_date,
+                SUM(BKP.quantity) AS booked_karts
+            FROM booked_places BKP
+                JOIN bookings BK ON BK.id = BKP.bookings_id
+            WHERE BK.status IN ('CONFIRMED', 'PENDING')
+            GROUP BY
+                BKP.tracks_id,
+                BKP.time_slots_id,
+                BK.booking_date
+        ) CAPTR ON CAPTR.tracks_id = TRK.id AND CAPTR.time_slots_id = TS.id
+
+    WHERE TRK.is_active = 1;
+
+CREATE OR REPLACE VIEW v_track_slots AS
+SELECT
+    TRK.id AS track_id,
+    TRK.name AS track_name,
+    TRK.max_karts,
+    TS.id AS time_slots_id,
+    TS.start_time,
+    TS.end_time
+FROM tracks TRK
+JOIN track_schedules SCH
+    ON SCH.tracks_id = TRK.id
+    AND SCH.is_open = 1
+JOIN time_slots TS
+    ON TS.id = SCH.time_slots_id
+WHERE TRK.is_active = 1;
