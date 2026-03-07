92efa69ef685ea9d9d70aad34703218f3b2c82f7
rv-bit <robertvilcu89@gmail.com>
2026-03-07 15:21:15 +0000

    chore: change comments

--- track_schedules/queries.sql
+++ track_schedules/queries.sql
@@ -1,6 +1,6 @@
 SET autocommit = 0; -- Causes for no auto committing in MySQL, shouldn't be used in Oracle
 
--- Simple update where id is 1, and commit changes
+-- Simple update where id is 12, and commit changes
 -- If id not found, rollbacks / raises error instantly
 START TRANSACTION;
 	UPDATE track_schedules SET is_open = false
@@ -11,7 +11,7 @@ START TRANSACTION;
 	SELECT * FROM track_schedules 
 	ORDER BY 1 ASC
 
--- -- Simple delete where first name is CJ, and commit changes
+-- -- Simple delete where id is 12, and commit changes
 START TRANSACTION;
 	DELETE FROM track_schedules WHERE id = 12;
 	COMMIT;
